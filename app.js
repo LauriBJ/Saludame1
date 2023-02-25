edecd537d33429eaf80879cc52b0109915bde9b1
LauriBJ <laurita.buttner@gmail.com>
2023-02-24 23:44:16 -0300

    saludando al link

---
+++ app.js
@@ -0,0 +1,15 @@
+/*Nombre: Laura BJ
+Misión: Crear una aplicación Web con Express con una única ruta GET / 
+que reciba una propiedad en el query string llamada nombre y muestre 
+un mensaje Hola seguido del valor de la propiedad y un !.
+*/
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  const nombre = req.query.nombre;
+  res.end("Hola " + nombre + "!");
+});
+
+app.listen(3000, () => console.log("escuchando"));
