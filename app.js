7dc7208787facbd57e494c5688503cb1c42766a3
LauriBJ <laurita.buttner@gmail.com>
2023-02-24 23:49:13 -0300

    agregando etiqueta h1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   const nombre = req.query.nombre;
-  res.end("Hola " + nombre + "!");
+  res.end("<h1>Hola " + nombre + "!</h1>");
 });
 
 app.listen(3000, () => console.log("escuchando"));
